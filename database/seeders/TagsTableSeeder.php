dcb313d4803d81beda93342d4d7e71766aa0d857
tangrowth <to.chinatsu@icloud.com>
2022-10-02 21:25:27 +0900

    シーダー編集

--- database/seeders/TagsTableSeeder.php
+++ database/seeders/TagsTableSeeder.php
@@ -15,15 +15,27 @@ class TagsTableSeeder extends Seeder
     public function run()
     {
         $param = [
-            'tag' => '衣装制作'
-        ];
-        Tag::create($param);
-        $param = [
             'tag' => '買い出し'
         ];
         Tag::create($param);
         $param = [
+            'tag' => '衣装作成'
+        ];
+        Tag::create($param);
+        $param = [
             'tag' => '衣装合わせ'
+        ];
+        Tag::create($param);
+        $param = [
+            'tag' => '反省'
+        ];
+        Tag::create($param);
+        $param = [
+            'tag' => '衣装管理'
+        ];
+        Tag::create($param);
+        $param = [
+            'tag' => 'その他'
         ];
         Tag::create($param);
     }
